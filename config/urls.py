27d900cb6831e69d8a4222ac7197e08713be8f3f
Yichen Pan <s301751752@gmail.com>
2025-06-29 05:54:51 +0800

    feat: redirect 404 and 404 page

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
+from django.shortcuts import render
 from django.urls import include, path
 
 urlpatterns = [
-    path('', include('press.urls')),
     path('admin/', admin.site.urls),
+    path('', include('press.urls')),
 ]
+
+
+def custom_404_view(request, exception):
+    return render(request, 'press/404.html', status=404)
+
+
+handler404 = 'config.urls.custom_404_view'
