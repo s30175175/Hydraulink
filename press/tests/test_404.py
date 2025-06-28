ff49905d895de24f6127d64c32c3b50c2d03b65c
Yichen Pan <s301751752@gmail.com>
2025-06-29 06:26:28 +0800

    feat: add 404 test

---
+++ press/tests/test_404.py
@@ -0,0 +1,13 @@
+import pytest
+
+
+@pytest.mark.django_db
+def test_404_status_code(client):
+    response = client.get('/thispagedoesnotexist/')
+    assert response.status_code == 404
+
+
+@pytest.mark.django_db
+def test_404_custom_template(client):
+    response = client.get('/no-such-url/')
+    assert '找不到頁面' in response.content.decode('utf-8')
