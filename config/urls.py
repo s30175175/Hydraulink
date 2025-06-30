ca8b22c5bbb1b214dc819706d0fde6316bf78486
Yichen Pan <s301751752@gmail.com>
2025-06-30 16:54:13 +0800

    fix: auto add superuser

--- config/urls.py
+++ config/urls.py
@@ -15,16 +15,3 @@ def custom_404_view(request, exception):
 
 
 handler404 = 'config.urls.custom_404_view'
-
-if settings.SUPERUSER_USERNAME and settings.SUPERUSER_PASSWORD:
-    from django.contrib.auth import get_user_model
-
-    User = get_user_model()
-
-    if not User.objects.filter(username=settings.SUPERUSER_USERNAME).exists():
-        print('Creating superuser...')
-        User.objects.create_superuser(
-            username=settings.SUPERUSER_USERNAME, password=settings.SUPERUSER_PASSWORD
-        )
-    else:
-        print('Superuser already exists.')
